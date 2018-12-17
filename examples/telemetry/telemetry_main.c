1fba1ed455f2ccb6b93281fd633d07a95e8ebe2e
Juan Flores <jfloresm92@gmail.com>
2018-12-17 14:40:17 +0100

    Minor modification in telemetry app

--- examples/telemetry/telemetry_main.c
+++ examples/telemetry/telemetry_main.c
@@ -1,8 +1,8 @@
 /****************************************************************************
  * examples/telemetry/telemetry_main.c
  *
- *   Copyright (C) 2018 Erle Robotics (Juan Flores Muñoz). All rights reserved.
- *   Author: Erle Robotics (Juan Flores Muñoz) <juan@erlerobotics.com>
+ *   Copyright (C) 2018 Acutronics Link Robotics. All rights reserved.
+ *   Author: Acutronics Link Robotics <juan@erlerobotics.com>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -33,83 +33,92 @@
  *
  ****************************************************************************/
 
- /****************************************************************************
-  * Included Files
-  ****************************************************************************/
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
 
-  #include <nuttx/config.h>
-  #include <stdio.h>
- #include <string.h>
- #include <stdlib.h>
- #include <fcntl.h>
- #include <unistd.h>
+#include <fcntl.h>
+#include <math.h>
+#include <nuttx/config.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
- /****************************************************************************
-  * Public Functions
-  ****************************************************************************/
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
 
-  /****************************************************************************
-   * Variables
-   ****************************************************************************/
-int i=0;
-int a=0;
-float e=0;
-FILE *fd_cpu;
-char cpuload_buf[6];
-char aux_c;
+/****************************************************************************
+ * Variables
+ ****************************************************************************/
 
- /****************************************************************************
-  * hello_main
-  ****************************************************************************/
+int a = 0;
+float e = 0;
 
- #ifdef CONFIG_BUILD_KERNEL
- int main(int argc, FAR char *argv[])
- #else
- int telemetry_main(int argc, char *argv[])
- #endif
- {
-   fd_cpu = fopen("/proc/cpuload", "r");
+char cpuload_buf[256];
+int aux_c;
+int lSize;
+char *buffer;
 
-   printf("Mounting file system \n\n");
-   system("mount -t procfs /proc");
-   sleep(1);
+/****************************************************************************
+ * hello_main
+ ****************************************************************************/
 
-   printf("Pushing the CPU to the limits\n");
-   printf("We will do the next floating operation: \n");
-   printf("e=3.141592653589793\n");
-   printf("e=e*e\n");
-   printf("This operation 20000 times\n");
-   sleep(5);
-   e=3.141592653589793;
-   while(1){
-     if(a==20000)break;
-     a++;
-     e= e*e;
-     while(1){
-       aux_c = fgetc(fd_cpu);
-       if( feof(fd_cpu) ) break;
-       cpuload_buf[i]=aux_c;
-       i++;
-     }
-     i=0;
-   }
-   printf("Max CPU load achieve: %s",cpuload_buf);
-   //TODO: Show properly the maximun CPU Load
-   printf("\nTest Finish\n");
+#ifdef CONFIG_BUILD_KERNEL
+int main(int argc, FAR char *argv[])
+#else
+int telemetry_main(int argc, char *argv[])
+#endif
+{
+  int i = 0;
+  printf("Mounting file system \n\n");
+  system("mount -t procfs /proc");
 
-   printf("Showing interruptions\n\n");
-   system("cat /proc/irqs");
-   sleep(2);
-   printf("Memory ussage\n\n");
-   system("cat /proc/meminfo");
-   sleep(2);
-   printf("Stack of the application\n\n");
-   system("cat /proc/self/stack");
-   sleep(2);
-   printf("Status of the running task\n\n");
-   system("cat /proc/self/status");
-   sleep(2);
-   printf("Type cat /proc/self/status\n");
-   printf("To see the context change after running this app\n");
-   return 0;
- }
+  FILE *fd_cpu;
+  fd_cpu = fopen("/proc/cpuload", "r");
+  if (fd_cpu < 0) return -1;
+  sleep(1);
+
+  printf("Pushing the CPU to the limits\n");
+  printf("We will do the next floating operation: \n");
+  printf("e=3.141592653589793\n");
+  printf("e=e*e\n");
+  printf("This operation 100000 times\n");
+  //   sleep(5);
+  e = 3.141592653589793;
+
+  while (i < 100000) {
+    e = e * e;
+    printf("=");
+    i++;
+  }
+  printf("\n");
+  printf("\nMax CPU load achieve: \n");
+  system("cat /proc/cpuload");
+
+  printf("\nCPU performace test finis!\n");
+
+  printf("Showing interruptions\n\n");
+  system("cat /proc/irqs");
+  sleep(2);
+
+  printf("Memory ussage\n\n");
+  system("cat /proc/meminfo");
+  sleep(2);
+
+  printf("Stack of the application\n\n");
+  system("cat /proc/self/stack");
+  sleep(2);
+
+  printf("Status of the running task\n\n");
+  system("cat /proc/self/status");
+  sleep(2);
+
+  printf("Type cat /proc/self/status\n");
+  printf("To see the context change after running this app\n");
+  return 0;
+
+  fclose(fd_cpu);
+}
